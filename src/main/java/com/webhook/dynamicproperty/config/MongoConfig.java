5e74c9f0ec41f16a09039baadfcc429e0ca57b1a
PriyanshuGagiya <priyanshuahir78@gmail.com>
2024-06-19 15:11:58 +0530

    chore: Update MongoDB driver dependency and configuration

--- src/main/java/com/webhook/dynamicproperty/config/MongoConfig.java
+++ src/main/java/com/webhook/dynamicproperty/config/MongoConfig.java
@@ -1,10 +1,11 @@
 package com.webhook.dynamicproperty.config;
 
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
-
 
 @Configuration
 public class MongoConfig {
@@ -12,13 +13,17 @@ public class MongoConfig {
     @Value("${custom.mongodb.url}")
     private String mongoUrl;
 
-    public MongoTemplate getMongoTemplateForDatabase()
-    {
-        return new MongoTemplate(new SimpleMongoClientDatabaseFactory(mongoUrl));
+    public MongoTemplate getMongoTemplateForDatabase() {
+        ConnectionString connectionString = new ConnectionString(mongoUrl);
+        MongoClientSettings mongoClientSettings = MongoClientSettings.builder()
+                .applyConnectionString(connectionString)
+                .build();
+
+        return new MongoTemplate(MongoClients.create(mongoClientSettings), "prod1"); // "prod1" is your database name
     }
+
     public String getMongoUrl() {
         return mongoUrl;
     }
-    
-}
 
+}
